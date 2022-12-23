81eaf09df72d1b68d342a233a8b88b971d047b66
Richard <charichard09@gmail.com>
2022-12-23 13:31:59 -0800

    WIP: search bar

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -5,11 +5,35 @@ namespace HairSalon.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly HairSalonContext _db;
+
+    public HomeController(HairSalonContext db)
+    {
+      _db = db;
+    }
 
     [Route("/")]
     public ActionResult Index() 
     {
       return View();
+    }
+
+    public async Task<IActionResult> Index(string searchString)
+    {
+      if (_db.Stylists == null)
+      {
+          return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+      }
+
+      Stylist stylist = from i in _db.Stylists
+                  select i;
+
+      if (!String.IsNullOrEmpty(searchString))
+      {
+          stylist = stylist.Where(s => s.Name!.Contains(searchString));
+      }
+
+      return View(await stylist.ToListAsync());
     }
   }
 }
