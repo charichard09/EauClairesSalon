59f713f562ba9eb2c2c6cec3177fce3f38013e00
Richard <charichard09@gmail.com>
2022-12-23 15:08:10 -0800

    WIP: add search bar for both clients and stylists

--- HairSalon/Controllers/HomeController.cs
+++ HairSalon/Controllers/HomeController.cs
@@ -27,12 +27,14 @@ namespace HairSalon.Controllers
       // Perform the search and retrieve the results
       List<Stylist> stylistResults = _db.Stylists.Where(s => s.Name.Contains(query)).ToList();
       List<Client> clientResults = _db.Clients.Where(c => c.Name.Contains(query)).ToList();
-      Dictionary<string, object> results = new Dictionary<string, object>();
-      ViewBag.stylists = stylistResults;
-      ViewBag.clients = clientResults;
+      Dictionary<string, object[]> results = new Dictionary<string, object[]>();
+      // ViewBag.stylists = stylistResults;
+      // ViewBag.clients = clientResults;
+      results.Add("stylists", stylistResults.ToArray());
+      results.Add("clients", clientResults.ToArray());
 
       // Return the view with the results
-      return RedirectToAction("Index");
+      return View("Index", results);
     }
   }
 }
