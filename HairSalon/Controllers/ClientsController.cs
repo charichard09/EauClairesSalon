5c12bda283f2fd5dab0c6f86d01d0b2ac2db04c2
Richard <charichard09@gmail.com>
2022-12-23 10:49:05 -0800

    add Client Index and view

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -16,6 +16,12 @@ public class ClientsController : Controller
     _db = db;
   }
 
+  public ActionResult Index()
+  {
+    List<Client> model = _db.Clients.Include(client => client.Stylist).ToList();
+    return View(model);
+  }
+
   public ActionResult Create()
   {
     ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
