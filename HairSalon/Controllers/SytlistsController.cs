6a16958acce2b41b64d30f8eab32d19171161ea1
Richard <charichard09@gmail.com>
2022-12-23 10:10:33 -0800

    add Stylist Create Index and view

--- HairSalon/Controllers/SytlistsController.cs
+++ HairSalon/Controllers/SytlistsController.cs
@@ -20,4 +20,17 @@ public class StylistsController : Controller
     List<Stylist> model = _db.Stylists.ToList();
     return View(model);
   }
+
+  public ActionResult Create()
+  {
+    return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Stylist stylist)
+  {
+    _db.Stylists.Add(stylist);
+    _db.SaveChanges();
+    return RedirectToAction("Index");
+  }
 }
